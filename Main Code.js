a0a769acb23bcb9c5b36ab6d832210642a1eb0b5
Geonwoo1109 <74478101+Geonwoo1109@users.noreply.github.com>
2021-10-31 21:37:20 +0900

    Update Main Code.js

--- Main Code.js
+++ Main Code.js
@@ -1,72 +1,127 @@
-const scriptName = "출첵";
+const scriptName = "출첵2";
 
 const Fs = FileStream;
-const Path = "/sdcard/attendance/";
+const Path = "/sdcard/Attendance2/";
 
-var list = {};
-var d_rank = 1;
+var file = {};
+var arr = [];
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-  
-  try{
-  
-  if (!Fs.read(Path + room)) {
-    Fs.write(Path + room, "{}");
-  }
-  
-  list = JSON.parse(Fs.read(Path + room));
-  
-  if (msg == ".ㅊㅊ") {
+  try {
+    if (room != "카톡봇 테스트방") return;
     
-    //replier.reply(JSON.stringify(Fs.read(Path + room), null, 4));
-    
-    if (Object.keys(list).includes(sender)) {
-      replier.reply("이미 출석체크를 하셨습니다!");
-    } else {
-      list[sender] = T();
-      replier.reply(sender + "님이 " + d_rank + " 등으로 출석하셨습니다!");
-      d_rank++;
+    if (msg == ".ㄹ") Fs.write(Path + room, "{}")
+    if (msg == ".ㄹㅇ") {
+      file = JSON.parse(Fs.read(Path + room));
+      file["today"] = [];
+          Fs.write(Path + room, JSON.stringify(file));
     }
     
-    Fs.write(Path + room, JSON.stringify(list));
-  }
-  
-  if (msg == ".ㅊㅊ순위") {
+    
+    if (!Fs.read(Path + room)) Fs.write(Path + room, "{}");
+    
+    file = JSON.parse(Fs.read(Path + room));
+    
+    if (!JSON.parse(Fs.read(Path + room))) file = {};
+    if (!JSON.parse(Fs.read(Path + room))["today"]) file["today"] = [];
+    if (!JSON.parse(Fs.read(Path + room))["total"]) file["total"] = [];
+    
+    Fs.write(Path + room, JSON.stringify(file));
     
     
-    //replier.reply(JSON.stringify(list, null, 4));
-    //replier.reply(Object.keys(list).length);
+    if (msg == ".ㅌ") {
+      replier.reply(JSON.stringify(file, null, 4))
+      replier.reply(JSON.stringify(file["total"].find(e => e.name == sender).score))
     
-    var finalList = [];
-    for (i=1; i<=Object.keys(list).length; i++) {
-      finalList.push(
-        i + "등: " + Object.keys(list)[i-1]
-        + "("+list[Object.keys(list)[i-1]]+")");
     }
     
-    replier.reply(
-    "<출석 순위>\n\n"
-    + finalList.join("\n"));
     
-  }
-  
-  if (msg == ".ㅊㅊ리셋") {
-    var list = {};
-    d_rank = 1;
-    Fs.write(Path + room, "{}");
-    replier.reply("출석 목록을 제거하였습니다.");
-  }
-  
-  
-  
-  
-  
+    if (msg == ".ㅊㅊ") {
+      if (!file["today"].find(e => e.name == sender)) {
+        //replier.reply(1)
+        var score = Math.floor(Math.random()*10) + 1;
+        
+        file["today"].push(
+          {
+            'name': sender,
+            'time': time(),
+            'point': score
+          }
+        );
+        
+        if (!file["total"].find(e => e.name == sender)) {
+          
+          file["total"].push(
+            {
+              'name': sender,
+              'score': score
+            }
+          );
+          
+        } else {
+          /*
+          var temp = file["total"][file["total"].find(e => e.name == sender)+1];
+          replier.reply(temp)
+          */
+          
+          file["total"].find(e => e.name == sender).score += score;
+          //file["total"][file["total"].find(e => e.name == sender)].score = temp + score;
+        }
+        
+        
+                
+        replier.reply(
+          ">" + sender + "님이\n" + file["today"].length + "등으로 출석하셨어요."
+          + "\n\n출석 확인: " + time()
+          + "얻은 점수: " + score);
+          
+          
+          
+        Fs.write(Path + room, JSON.stringify(file, null, 4));
+          
+      } else {
+        
+        replier.reply(sender + "님은 이미 출석하셨슴니다!"
+          + "\n최근 출석: " + file["today"].find(e => e.name == sender).time);
+      }
+      
+      
+    }
+    
+    if (msg == ".오늘순위") {
+      
+      arr = [];
+      
+      for (i=0; i<file["today"].length; i++) {
+        arr.push(
+          (i+1).toString() + "등: " + file["today"][i].name
+          + "\n시간: " + file["today"][i].time
+        );
+      }
+      
+      replier.reply("오늘순위\n\n" + arr.join("\n\n"));
+    }
+    
+    if (msg == ".전체순위") {
+      
+      arr = [];
+      
+      
+      
+      replier.reply(Object.keys(file["total"]))
+    }
+    
   } catch(e) {
-    replier.reply(e+e.lineNumber);
+    replier.reply(e + e.lineNumber);
   }
 }
 
-function T() {
+
+function time() {
   var day = new Date();
-  return day.getHours() + "h" + day.getMinutes() + "m" + day.getSeconds() + "s";
+  return (day.getHours() > 12 ? "오후 "
+      + (day.getHours() -12).toString() : day.getHours().toString()) + "시 "
+    + day.getMinutes() + "분 "
+    + day.getSeconds() + "초";
+  
 }
